f8b2f1dcbc23e0bf7753bd8d6c079235a2dfc706
Vladimir Litvinov <litvinovvn@rambler.ru>
2023-12-05 19:10:41 +0300

    masterWorkers

--- masterWorkers.cpp
+++ masterWorkers.cpp
@@ -5,16 +5,26 @@
 #include <thread>
 #include <mutex>
 
-const unsigned numWorkerThreads = 3;
+const unsigned numWorkerThreads = 4;
 
 std::mutex mutex;
-
 bool startWorking = false;
+bool isWorkersReady[numWorkerThreads];
+std::mutex isWorkersReady_mutex;
 
-void workerFunc(int tid)
+void printIsWorkersReady()
 {
-    printf("[worker %d] Started\n", tid);
+    isWorkersReady_mutex.lock();
+    printf("isWorkersReady: ");
+    for(int i = 0; i < numWorkerThreads; i++)
+        printf("%d ", isWorkersReady[i]);
+    printf("\n");
+    isWorkersReady_mutex.unlock();
+}
 
+// Ожидание команды начала работы
+inline void waitingStartWorkingCommand()
+{
     while (true)
     {
         bool isBreak = false;
@@ -24,31 +34,75 @@ void workerFunc(int tid)
 
         if(isBreak) break;
     }
-    
+}
+
+inline void setIsWorkerReady(int tid)
+{
+    isWorkersReady_mutex.lock();
+    isWorkersReady[tid] = true;    
+    isWorkersReady_mutex.unlock();
+}
+
+void workerFunc(int tid)
+{
+    printf("[worker %d] Started\n", tid);
+    waitingStartWorkingCommand();
+        
     printf("[worker %d] Working\n", tid);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    
+    setIsWorkerReady(tid);
     
 
+
     printf("[worker %d] Ended\n", tid);
+}
+
+inline void setStartWorking(bool value)
+{
+    mutex.lock();
+    printf("[manager] Start calculation command: %d\n", value);
+    startWorking = value;
+    mutex.unlock();
+}
+
+inline void waitingAllWorkersReady()
+{
+    // Проверяем, что текущий этап работ выполнен всеми потоками-исполнителями
+    while (true)
+    {
+        bool isBreak = true;
+        isWorkersReady_mutex.lock();
+        for(int i = 0; i < numWorkerThreads; i++)
+            if(!isWorkersReady[i]) isBreak=false;
+        isWorkersReady_mutex.unlock();
+
+        if(isBreak) break;
+    }
 }
 
 void managerFunc()
 {
     printf("[manager] Started\n");
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    printIsWorkersReady();
+    //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-    mutex.lock();
-    printf("[manager] Start calculation command\n");
-    startWorking=true;
-    mutex.unlock();
-    // Проверяем, что текущий этап работ выполнен всеми потоками-исполнителями
+    setStartWorking(true);
+    
+    waitingAllWorkersReady();
+    
+    printf("[manager] Step 1 completed!\n");
+    printIsWorkersReady();
 
-
-
-    printf("[manager] Ended\n");
+    printf("[manager] All work completed!\n");
 }
 
 int main()
 {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
     printf("Starting working threads\n");
     std::vector<std::thread> threads;
 
@@ -56,14 +110,16 @@ int main()
         threads.push_back(std::thread(workerFunc,i));
 
     threads.push_back(std::thread(managerFunc));
-
     
-
     for(auto& thread : threads)
     {
         if(thread.joinable())
             thread.join();
     }
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("cpu_time_used = %lf sec\n", cpu_time_used);
 
     return 0;
 }
