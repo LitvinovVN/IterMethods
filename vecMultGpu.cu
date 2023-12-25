9e29480475df6d1ddc97faa59e61343423f47cfd
litvinovvn <litvinovvn@rambler.ru>
2023-12-25 15:18:14 +0300

    cuda vec mult seq

--- vecMultGpu.cu
+++ vecMultGpu.cu
@@ -7,30 +7,59 @@
 
 #include <stdio.h>
 
-cudaError_t vecMultCuda(int *c, const int *a, const int *b, unsigned int size);
+cudaError_t vecMultCuda(const vector1d *v1, const vector1d *v2, double* result);
 
-__global__ void vecMultKernel(int *c, const int *a, const int *b)
+__global__ void vecMultKernel(vector1d *v1, vector1d *v2, double* result)
 {
     int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+    //c[i] = a[i] + b[i];
+    printf("GPU v1: Length = %d\n", v1->length);
+    printf("GPU v1: Data[0] = %lf\n", v1->data[0]);
+    printf("GPU v2: Length = %d\n", v2->length);
+    printf("GPU v2: Data[0] = %lf\n", v2->data[0]);
+
+    double res = 0;
+    for(size_t i = 0; i < v1->length; i++)
+        res += v1->data[i] * v2->data[i];
+
+    *result = res;
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+    size_t size = 10;
+    
+    vector1d *v1 = vector1d_create(size); 
+    vector1d *v2 = vector1d_create(size);
+
+    double k = 0.5;
+    int min = 10;
+    int max = 20;
+
+
+    clock_t start, end;
+    double cpu_time_used;
+        
+    vector1d_init_scalar(v1, v2, k, min, max);
+    vector1d_print(v1, "v1 = ");
+    vector1d_print(v2, "v2 = ");
+    
+    start = clock();
 
     // Вычисление скалярного произведения
-    cudaError_t cudaStatus = vecMultCuda(c, a, b, arraySize);
+    double scalar_mult_result = 0;
+    cudaError_t cudaStatus = vecMultCuda(v1, v2, &scalar_mult_result);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "vecMultCuda failed!");
         return 1;
     }
+    
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("cpu_time_used = %lf sec\n", cpu_time_used);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+    printf("scalar_mult_result = %lf", scalar_mult_result);
+
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -43,12 +72,12 @@ int main()
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t vecMultCuda(int *c, const int *a, const int *b, unsigned int size)
+// Helper function for using CUDA to multiply vectors in parallel.
+cudaError_t vecMultCuda(const vector1d *v1, const vector1d *v2, double* result)
 {
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
+    vector1d *dev_v1 = 0;
+    vector1d *dev_v2 = 0;
+    double *dev_result = 0;
     cudaError_t cudaStatus;
 
     // Выбор GPU для запуска, change this on a multi-GPU system.
@@ -58,40 +87,69 @@ cudaError_t vecMultCuda(int *c, const int *a, const int *b, unsigned int size)
         goto Error;
     }
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    // Allocate GPU buffers for three vectors (two input, one output)
+    double *dev_v1_data = 0;
+    cudaStatus = cudaMalloc((void**)&dev_v1_data, v1->length * sizeof(double));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }    
+    cudaStatus = cudaMalloc((void**)&dev_v1, sizeof(vector1d));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
+    vector1d v1_dto;
+    v1_dto.length = v1->length;
+    v1_dto.data = dev_v1_data;
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+
+    double *dev_v2_data = 0;
+    cudaStatus = cudaMalloc((void**)&dev_v2_data, v2->length * sizeof(double));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
+    cudaStatus = cudaMalloc((void**)&dev_v2, sizeof(vector1d));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+    vector1d v2_dto;
+    v2_dto.length = v2->length;
+    v2_dto.data = dev_v2_data;
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_result, sizeof(double));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
     // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaStatus = cudaMemcpy(dev_v1_data, v1->data, v1->length * sizeof(double), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+    cudaStatus = cudaMemcpy(dev_v1, &v1_dto, sizeof(vector1d), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaStatus = cudaMemcpy(dev_v2_data, v2->data, v2->length * sizeof(double), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+    cudaStatus = cudaMemcpy(dev_v2, &v2_dto, sizeof(vector1d), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
     }
 
     // Launch a kernel on the GPU with one thread for each element.
-    vecMultKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+    vecMultKernel<<<1, 1>>>(dev_v1, dev_v2, dev_result);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -109,16 +167,16 @@ cudaError_t vecMultCuda(int *c, const int *a, const int *b, unsigned int size)
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(result, dev_result, sizeof(double), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
-    }
+    } // */
 
 Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
+    cudaFree(dev_v1);
+    cudaFree(dev_v2);
+    cudaFree(dev_result);
     
     return cudaStatus;
 }
